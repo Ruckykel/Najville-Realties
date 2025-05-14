d7d07a028a4bd98bc4ae06d14bb5a46ad5294c97
Ruckykel <ruckykel1@gmail.com>
2025-05-14 12:22:04 +0100

    cda

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import Home from './components/Home/Home';
 import Services from './components/Services/Services';
 import Contact from './components/Contact/Contact';
 import Training from './components/Training/Training';
+import PageTracker from './PageTracker'; // Add this import
 
 function App() {
   return (
     <Router>
+      <PageTracker /> {/* Add this inside Router */}
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/Services" element={<Services/>} />
-        <Route path="/Training" element={<Training/>} />
-        <Route path="/Contact" element={<Contact/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Training" element={<Training />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
